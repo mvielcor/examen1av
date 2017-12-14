3c1b4efdf2c0b34ea7e254a6e6f967766197a90d
mvielcor <mvielcor@gmail.com>
2017-12-14 19:29:57 +0100

    Pujada de tot el projecte

--- app/src/main/java/pmdm/mviel/examen1av/Activity2.java
+++ app/src/main/java/pmdm/mviel/examen1av/Activity2.java
@@ -3,11 +3,26 @@ package pmdm.mviel.examen1av;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements Botonera.InteraccioAmbBotonera {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    @Override
+    public void carrregaDadesPersonalsFragment() {
+
+    }
+
+    @Override
+    public void carregaAficionsFragment() {
+
+    }
+
+    @Override
+    public void carregarSubactivity() {
+
     }
 }
